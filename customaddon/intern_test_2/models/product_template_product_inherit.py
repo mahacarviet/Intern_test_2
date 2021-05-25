3c5b855019521bac4b39ab7dccffd39f8b357cf6
Viet <11185573@st.neu.edu.vn>
2021-05-25 15:49:35 +0700

    ODOO_RM_INTERN-049: Create custom field to store the product warranty

--- customaddon/intern_test_2/models/product_template_product_inherit.py
+++ customaddon/intern_test_2/models/product_template_product_inherit.py
@@ -1,22 +1,20 @@
 from odoo import fields, models, api
-import datetime
+from datetime import datetime
 
 class ProductTemplateProductInherit(models.Model):
-    _inherit = 'product.template.product'
+    _inherit = 'product.template'
 
     date_to = fields.Date()
     date_from = fields.Date()
-    to_date = fields.Char(date_to.strftime("%d%m%y"))
-    from_date = fields.Char(date_from.strftime("%d%m%y"))
     product_warranty = fields.Text(compute="_compute_product_warranty", store=True)
     check_product_warranty = fields.Boolean(compute="_compute_check_product_warranty", store=True)
 
     @api.depends('date_to', 'date_from')
-    def _compute_check_discount_code(self):
+    def _compute_product_warranty(self):
         for rec in self:
             if (rec.date_to == '') or (rec.date_from == ''):
                 if rec.date_from < rec.date_to:
-                    rec.product_warranty = "PWR/" + rec.from_date + "/" + rec.to_date
+                    rec.product_warranty = "PWR/" + rec.date_from.strftime("%d%m%y") + "/" + rec.date_to.strftime("%d%m%y")
                 else:
                     rec.product_warranty = ''
             else:
@@ -26,9 +24,9 @@ class ProductTemplateProductInherit(models.Model):
     def _compute_check_product_warranty(self):
         for rec in self:
             if rec.product_warranty != '':
-                rec.check_discount_code = True
+                rec.check_product_warranty = True
             else:
-                rec.check_discount_code = False
+                rec.check_product_warranty = False
 
 
 
